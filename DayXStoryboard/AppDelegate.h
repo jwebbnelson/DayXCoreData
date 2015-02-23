15c2909a3e305a28556905ce14bbdf50223a5902
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-02-23 13:59:56 -0700

    Initial Commit

---
+++ DayXStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayXStoryboard
+//
+//  Created by Jordan Nelson on 2/23/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
